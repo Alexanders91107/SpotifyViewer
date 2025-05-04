28206c0270405f681f93156ca8ccd87f76402b54
Alexanders91107 <Alexanders91107@gmail.com>
2025-05-03 23:41:43 -0400

    fixed loop

--- src/main.js
+++ src/main.js
@@ -1,37 +1,3 @@
-import Graph from 'graphology';
-import Sigma from 'sigma';
-/*
-const clientId = '77456fd240024ae29f080233a70335b0';
-const redirectUri = window.location.origin;
-const scopes = 'user-read-private';
-
-function getAccessToken() {
-  const hash = window.location.hash;
-  if (!hash) return null;
-  const params = new URLSearchParams(hash.substring(1));
-  return params.get('access_token');
-}
-
-function redirectToSpotifyAuth() {
-  const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
-  window.location.href = authUrl;
-}
-
-const token = getAccessToken();
-
-function getSpotifyProfile(token) {
-  return fetch('https://api.spotify.com/v1/me', {headers: {Authorization: `Bearer ${token}`,}}).then(res => res.json())
-}
-
-if (!token){redirectToSpotifyAuth();} 
-else {
-  getSpotifyProfile(token).then(userData => {
-    // You can use userData here
-    console.log('Email:', userData.email);
-  });
-}
-*/
-
 const clientId = "77456fd240024ae29f080233a70335b0"; // Replace with your client ID
 const code = undefined;
 
@@ -52,7 +18,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", window.location.origin);
+  params.append("redirect_uri", 'https://effulgent-narwhal-d486fb.netlify.app/main');
   params.append("scope", "user-read-private user-read-email");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -106,11 +72,3 @@ async function fetchProfile(token) {
 
   return await result.json();
 }
-
-const graph = new Graph();
-
-graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
-graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
-graph.addEdge("1", "2", { size: 5, color: "purple" });
-
-const sigmaInstance = new Sigma(graph, document.getElementById("sigma-container"));
