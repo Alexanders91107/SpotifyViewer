7d5b9c456f2c00176dbf1f5d04200bc061acb4d5
Alexanders91107 <Alexanders91107@gmail.com>
2025-05-03 23:53:05 -0400

    more fixes

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", 'https://effulgent-narwhal-d486fb.netlify.app/main');
+  params.append("redirect_uri", 'https://effulgent-narwhal-d486fb.netlify.app/main.html');
   params.append("scope", "user-read-private user-read-email");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
