d91fe05b5f0542dd0399321836e10a7bb7c8b73b
Alexanders91107 <Alexanders91107@gmail.com>
2025-05-03 23:55:04 -0400

    more fixes

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ export async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://127.0.0.1:5173/callback");
+  params.append("redirect_uri", "https://effulgent-narwhal-d486fb.netlify.app/main.html");
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
