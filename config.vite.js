dedf18ecb5444fa4bcb3db64b938a3b3ee195b80
Alexanders91107 <Alexanders91107@gmail.com>
2025-05-04 22:48:22 -0400

    added redirect

--- config.vite.js
+++ config.vite.js
@@ -1,5 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/', // keeps your base path config
+  build: {
+    target: 'es2022' // enables top-level await support
+  }
 });
