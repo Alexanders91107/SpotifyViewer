82d02b561d61ffca055ea229821ad63e39780032
Alexanders91107 <Alexanders91107@gmail.com>
2025-05-05 15:21:08 -0400

    added redirect

--- src/main.js
+++ src/main.js
@@ -99,7 +99,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   params.append("client_id", clientId);
   params.append("response_type", "code");
   params.append("redirect_uri", 'https://effulgent-narwhal-d486fb.netlify.app/main');
-  params.append("scope", "user-read-private user-read-email");
+  params.append("scope", "user-read-private user-read-email user-top-read");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
 
